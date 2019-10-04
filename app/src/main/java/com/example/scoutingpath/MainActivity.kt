a9cf20d76280cac5a6e65b5fa4d3ffc6970745cc
Annika Larson <annika@cole-larson.org>
2019-10-04 16:47:56 -0700

    Display XY coordinates.

--- app/src/main/java/com/example/scoutingpath/MainActivity.kt
+++ app/src/main/java/com/example/scoutingpath/MainActivity.kt
@@ -3,6 +3,15 @@ package com.example.scoutingpath
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.pm.ActivityInfo
+import android.widget.Toast
+import android.view.MotionEvent
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,4 +19,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        // code from https://xjaphx.wordpress.com/2011/06/13/detect-xy-coordinates-when-clicking-or-touching-on-screen/
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val x = event.x
+            val y = event.y
+            xy.setText("X: " + x + " Y: " + y)
+        }
+        return super.onTouchEvent(event)
+    }
+
 }
