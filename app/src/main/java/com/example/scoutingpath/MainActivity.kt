cf46bf0ad6c1097893c98c1f7f2bfe9e48c75a6c
Annika Larson <annika@cole-larson.org>
2019-10-04 18:03:55 -0700

    might be broken

--- app/src/main/java/com/example/scoutingpath/MainActivity.kt
+++ app/src/main/java/com/example/scoutingpath/MainActivity.kt
@@ -6,28 +6,29 @@ import android.content.pm.ActivityInfo
 import android.graphics.Color
 import android.view.MotionEvent
 import android.widget.ImageView
-import android.widget.RelativeLayout
-import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 
 
 class MainActivity : AppCompatActivity() {
-    val X_OFFSET = 423 // X offset so that x,y are relative to the field.
-    val Y_OFFSET = 393 // Y offset so that x,y are relative to the field
+    val X_OFFSET = 50 // X offset so that x,y are relative to the field.
+    val Y_OFFSET = 50 // Y offset so that x,y are relative to the field
     override fun onCreate(savedInstanceState: Bundle?) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fieldImage.setX(  X_OFFSET.toFloat() ) // set the field image to be in the same place
+        fieldImage.setY( Y_OFFSET.toFloat() )
+
     }
     override fun onTouchEvent(event: MotionEvent): Boolean {
         // code from https://xjaphx.wordpress.com/2011/06/13/detect-xy-coordinates-when-clicking-or-touching-on-screen/
         // and https://stackoverflow.com/questions/3294590/set-the-absolute-position-of-a-view
         // and a lot of other places on the internet
         if (event.action == MotionEvent.ACTION_DOWN) { // It's a tap
-            val x = event.x - X_OFFSET // Correct for the field image placement
-            val y = event.y - Y_OFFSET
+            val x = event.x //- X_OFFSET // Correct for the field image placement
+            val y = event.y //- Y_OFFSET
 
             xy.setText("X: " + x + " Y: " + y) // update debug x,y
 
