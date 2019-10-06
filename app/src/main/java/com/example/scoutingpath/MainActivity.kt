93c7a7cd19cfbdbc652b972355d48d9776838a40
Annika Larson <annika@cole-larson.org>
2019-10-06 14:32:06 -0700

    Added magic numbers for x/y correction.

--- app/src/main/java/com/example/scoutingpath/MainActivity.kt
+++ app/src/main/java/com/example/scoutingpath/MainActivity.kt
@@ -12,14 +12,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    val X_OFFSET = 50 // X offset so that x,y are relative to the field.
-    val Y_OFFSET = 50 // Y offset so that x,y are relative to the field
+    var X_OFFSET = 0
+    var Y_OFFSET = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fieldImage.setX(  X_OFFSET.toFloat() ) // set the field image to be in the same place
-        fieldImage.setY( Y_OFFSET.toFloat() )
 
     }
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -27,17 +25,25 @@ class MainActivity : AppCompatActivity() {
         // and https://stackoverflow.com/questions/3294590/set-the-absolute-position-of-a-view
         // and a lot of other places on the internet
         if (event.action == MotionEvent.ACTION_DOWN) { // It's a tap
-            val x = event.x //- X_OFFSET // Correct for the field image placement
-            val y = event.y //- Y_OFFSET
+            // Compute x,y offsets
+            var location = IntArray(size=2)
+            fieldImage.getLocationOnScreen(location);
+            print(location)
+            X_OFFSET = location[0] + 102 // X offset so that x,y are relative to the field.
+            // 102 for the width of the off-field part of fieldImage
+            Y_OFFSET = location[1] + 26 + 32 // Y offset so that x,y are relative to the field
+            // magic numbers zzzz
+            val x = event.x - X_OFFSET// Correct for the field image placement
+            val y = event.y - Y_OFFSET
 
-            xy.setText("X: " + x + " Y: " + y) // update debug x,y
+            xy.setText("X: " + x + " Y: " + y +" " + X_OFFSET + "/" + Y_OFFSET) // update debug x,y
 
             val layout  = R.layout.activity_main //
             var iv: ImageView
             iv = ImageView(this)
             c.addView(iv)
             iv.x = event.x // event.x is a screen coordinate, whereas x is a corrected field coordinate.
-            iv.y = event.y - 180 // no clue why in the name of Android this is needed, but it is...
+            iv.y = event.y - 100 // no clue why in the name of Android this is needed, but it is...
             iv.layoutParams.height = 10
             iv.layoutParams.width = 10
             iv.setBackgroundColor(Color.RED)
